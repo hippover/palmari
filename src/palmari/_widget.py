81d8df3c8296cd7a865790da1e1558fe33c1ae8a
Hippolyte Verdier <hippover@orange.fr>
2023-01-10 12:24:05 +0100

    Lali

--- src/palmari/_widget.py
+++ src/palmari/_widget.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from .processing.tif_pipeline_widget import TifPipelineWidget
 from .processing.tif_pipeline import TifPipeline
 from .processing.steps import *
@@ -22,7 +24,7 @@ class PalmariWidget(TifPipelineWidget):
                 },
                 "tracker": {
                     "DiffusionTracker": {
-                        "max_diffusivity": 5.0,
+                        "max_diffusivityxz": 5.0,
                         "y_diff": 0.9,
                         "init_cost": 50.0,
                         "d_bound_naive": 0.1,
